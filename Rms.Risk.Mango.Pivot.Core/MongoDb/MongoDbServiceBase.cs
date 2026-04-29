d23ab183fda30056fd2ef4f85cbe06cdea7abc49
Alexander Shabarshov <alexander.shabarshov@db.com>
2026-04-29 09:55:15 +0100

    RSK-18485 Reduce logging

--- Rms.Risk.Mango.Pivot.Core/MongoDb/MongoDbServiceBase.cs
+++ Rms.Risk.Mango.Pivot.Core/MongoDb/MongoDbServiceBase.cs
@@ -35,9 +35,11 @@ internal static class FetchInfo
 
 public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
 {
-    // ReSharper disable once StaticMemberInGenericType
+    // ReSharper disable StaticMemberInGenericType
     // ReSharper disable once InconsistentNaming
     protected static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod()!.DeclaringType!);
+    public static bool Quiet = false;
+    // ReSharper restore StaticMemberInGenericType
 
     protected readonly IMongoCollection<T> Collection;
     private readonly   MongoDbSettings     _settings;
@@ -178,12 +180,14 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
         {
             var sw = Stopwatch.StartNew();
 
-            _log.Debug($"Id={id:000} Starting Find (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Filter=\"{jsonFilter}\"");
+            if ( !Quiet ) 
+                _log.Debug($"Id={id:000} Starting Find (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Filter=\"{jsonFilter}\"");
 
             var cursor = await Collection.FindAsync(filter, options, token);
 
             if (sw.Elapsed > TimeSpan.FromSeconds(10))
-                _log.Debug($"Id={id:000} Slow Find (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Elapsed=\"{sw.Elapsed:g}\" Filter=\"{jsonFilter}\"");
+                if ( !Quiet ) 
+                    _log.Debug($"Id={id:000} Slow Find (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Elapsed=\"{sw.Elapsed:g}\" Filter=\"{jsonFilter}\"");
 
             sw.Restart();
 
@@ -196,7 +200,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
                 {
                     if (!await cursor.MoveNextAsync(token))
                     {
-                        _log.Debug($"Id={id:000} Find complete (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Docs={processed} " +
+                        if ( !Quiet ) 
+                            _log.Debug($"Id={id:000} Find complete (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Docs={processed} " +
                                    $"Elapsed=\"{sw.Elapsed:g}\" DocsSec={processed / (sw.ElapsedMilliseconds / 1000.0):0.00} Filter=\"{jsonFilter}\"");
                         yield break;
                     }
@@ -219,7 +224,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
                         continue;
 
                     var dps = ReportEveryNDocuments / ((sw.ElapsedMilliseconds - prevBatchElapsed.TotalMilliseconds) / 1000.0);
-                    _log.Debug($"Id={id:000} Fetch {ReportEveryNDocuments:N0} documents (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} " +
+                    if ( !Quiet )
+                        _log.Debug($"Id={id:000} Fetch {ReportEveryNDocuments:N0} documents (no retries) Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} " +
                                $"Elapsed=\"{sw.Elapsed - prevBatchElapsed:g}\" DocsSec={dps:0.00} (processed {processed:N0} so far)");
                     prevBatchElapsed = sw.Elapsed;
                 }
@@ -264,7 +270,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
 
             var sw = Stopwatch.StartNew();
 
-            _log.Debug($"Id={id:000} Starting Find Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Filter=\"{jsonFilter}\"");
+            if ( !Quiet ) 
+                _log.Debug($"Id={id:000} Starting Find Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} Filter=\"{jsonFilter}\"");
 
             //var coll = Collection.Database.GetCollection<BsonDocument>(CollectionName);
 
@@ -327,7 +334,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
                             continue;
 
                         var dps = ReportEveryNDocuments / ((sw.ElapsedMilliseconds - prevBatchElapsed.TotalMilliseconds) / 1000.0);
-                        _log.Debug($"Id={id:000} Fetch {ReportEveryNDocuments:N0} documents Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} " +
+                        if ( !Quiet ) 
+                            _log.Debug($"Id={id:000} Fetch {ReportEveryNDocuments:N0} documents Collection=\"{CollectionName}\" Concurrency={FetchInfo.ParallelFinds} " +
                                    $"Elapsed=\"{sw.Elapsed - prevBatchElapsed:g}\" DocsSec={dps:0.00} (processed {processed:N0} so far)");
                         prevBatchElapsed = sw.Elapsed;
 
@@ -399,7 +407,7 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
                 throw;
         }
 
-        //_log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Inserted={dataList.Count - replaced - ignored} Replaced={replaced} Ignored={ignored} of Total={dataList.Count}");
+        //if ( !Quiet ) _log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Inserted={dataList.Count - replaced - ignored} Replaced={replaced} Ignored={ignored} of Total={dataList.Count}");
         return dataList.Count - ignored;
     }
 
@@ -437,7 +445,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
             : new FilterDefinitionBuilder<T>().Eq(cobName, cob.Date);
 
         var result = await Collection.DeleteManyAsync(filter, token);
-        _log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Deleted={result.DeletedCount} Book=\"{book}\" COB=\"{cob:yyyy-MM-dd}\" Layer=\"{layer}\"");
+        if ( !Quiet ) 
+            _log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Deleted={result.DeletedCount} Book=\"{book}\" COB=\"{cob:yyyy-MM-dd}\" Layer=\"{layer}\"");
     }
 
     public static Dictionary<string, object> ConvertBsonToDictionary(BsonDocument doc)
@@ -525,7 +534,8 @@ public abstract class MongoDbServiceBase<T> : IMongoDbService<T> where T : class
     public async Task<long> Delete(FilterDefinition<T> filter, CancellationToken token = default)
     {
         var result = await Collection.DeleteManyAsync(filter, token);
-        _log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Deleted={result.DeletedCount} Filter:\n{filter.ToJson(new () {Indent = true})}");
+        if ( !Quiet ) 
+            _log.Debug($"Collection=\"{Collection.CollectionNamespace.CollectionName}\" Deleted={result.DeletedCount} Filter:\n{filter.ToJson(new () {Indent = true})}");
         return result.DeletedCount;
     }
 
